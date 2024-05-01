54ca582e1fec0e8c565f5dcee69fe2c306eb2bce
mchekalov <mchekalov@gmail.com>
2024-05-01 23:56:24 +0500

    add storage sqlite

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,11 +2,44 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 	"urlshortener/internal/config"
+	"urlshortener/internal/lib/logger/sl"
+	"urlshortener/internal/storage/sqlite"
+)
+
+const (
+	localEnv = "local"
+	devEnv   = "dev"
+	prodEnv  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
+	log := setupLogger(cfg.Env)
+	log.Info("starting server", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 
+	stor, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("Failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	_ = stor
 	fmt.Println(cfg)
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case localEnv:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case devEnv:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case prodEnv:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+	return log
 }
